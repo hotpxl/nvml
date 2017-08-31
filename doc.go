903a0a6279bf613a5cf57eeabb1b105d70580c0a
Yutian Li <hotpxless@gmail.com>
2017-08-30 21:58:50 -0700

    [master] Add package documentation.

---
+++ doc.go
@@ -0,0 +1,36 @@
+/*
+Package nvml is the NVIDIA Management Library (NVML) bindings for Go.
+
+Following is an easy example that displays processes information on all devices.
+
+	package main
+
+	import (
+		"fmt"
+
+		"github.com/hotpxl/nvml"
+	)
+
+	func main() {
+		s, err := nvml.NewSession()
+		if err != nil {
+			panic(err)
+		}
+		defer s.Close()
+
+		devices, err := s.GetAllDevices()
+		if err != nil {
+			panic(err)
+		}
+		for _, d := range devices {
+			p, err := d.Processes()
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("%+v\n", p)
+		}
+	}
+
+Visit https://github.com/hotpxl/nvml for more information.
+*/
+package nvml
